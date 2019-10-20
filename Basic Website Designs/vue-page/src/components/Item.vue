2235e3775df35dcb1dce6b4f8a321bd72abc08e3
AlexandreHiroyuki <alexandrehiroyuki@hotmail.com>
2019-10-20 00:43:40 -0300

    Add Vue example page

---
+++ Basic Website Designs/vue-page/src/components/Item.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="db ma2 w-30 bg-light-green br2 grow-hover">
+    <h3>{{ index }}. {{ item }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  props: {
+    index: Number,
+    item: String
+  }
+};
+</script>
+
+<style>
+</style>
