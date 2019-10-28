f32c36bc48ed72cfac94cfa146f111d6a784ea4b
Mojtaba <mojtabazarerazi@gmail.com>
2019-10-28 11:36:22 +0330

    DevHelpBox logo animation

---
+++ Create Animation Designs/MZ-DevHelpBox logo animation/js/index.js
@@ -0,0 +1,34 @@
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 1200
+  }).add({
+    targets: '.ml5 .line',
+    duration: 1200,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["1.5em", 0],
+    easing: "easeOutExpo",
+    duration: 3600,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  });
